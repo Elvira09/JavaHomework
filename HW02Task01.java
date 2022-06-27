7608cfbfd9ecc2a945047b7596e15c7027d2705b
Elvira09 <zag0909@gmail.com>
2022-06-27 19:40:46 +0300

    HW02

--- HW02Task01.java
+++ HW02Task01.java
@@ -1,17 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 // У вас есть отсортированный массив (Можете сами сгенерировать и использовать 
 // любой удобный вам алгоритм сортировки (кроме пузырька)), нужно используя 
 // минимальное колличество попыток найти загаданное число (Алгоритм бинарного поиска )
 
-import java.util.Arrays;
+
+    // ВЫВОД
+        // Стартовый массив   [928, 209, 787, 800, 618, 517, 856, 520, 141, 766]
+        // Отсортированный массив   [141, 209, 520, 517, 618, 766, 787, 800, 856, 928]
+        // Введите число для поиска: 
+        // 787
+        // Искомый элемент 787 имеет индекс 6
 
 public class HW02Task01 {
+    // генерация случайного числа в заданном диапазоне
+    public static int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        int number = random.nextInt(max - min) + min;
+        return number;
+    }
+
+    // заполнение массива случайными числами в заданном диапозоне
+    public static int [] fillingArrayRandomNumbers(int length, int min, int max){
+        int[] array = new int [length]; 
+        for (int index = 0; index < array.length; index++) {
+            array[index] = getRandomNumber(min, max);
+        }
+        return array;
+    }
+
     // быстрая сортировка
     public static void quickSort(int[] array, int start, int end) {
-        if (array.length == 0 || start >= end)
-            return;
+        if (array.length == 0 || start >= end) return;
         
         int middle = start + (end - start) / 2;
         int supportElement = array[middle];
+
         int i = start;
         int j = end;
 
@@ -30,6 +56,15 @@ public class HW02Task01 {
         if (j > start) quickSort(array, i, end);
     }
 
+    // считывания числовых данных из терминала
+    public static int ScannerNumber() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Введите число для поиска: ");
+        int number = iScanner.nextInt();
+        iScanner.close();
+        return number;
+    }
+
     // бинарный поиск
     public static int binarySearch(int arr[], int elementToSearch) {
         int firstIndex = 0;
@@ -44,15 +79,17 @@ public class HW02Task01 {
     }
 
     public static void main(String[] args) {
-        int[] arrayStart = {344, 67, 90, 4, 78, 2, 0, 56, 789, 123}; 
-        int elementToSearch = 123;
+        int[] arrayStart = fillingArrayRandomNumbers(10, 2, 999);
         System.out.println("Стартовый массив   " + Arrays.toString(arrayStart)); 
         
         int start = 0;
         int end = arrayStart.length - 1; 
         quickSort(arrayStart, start, end);
-        System.out.println("Итоговый массив   " + Arrays.toString(arrayStart)); 
-
-        System.out.println("Искомый элемент " +  elementToSearch + " имеет индекс " + binarySearch(arrayStart, elementToSearch));
+        System.out.println("Отсортированный массив   " + Arrays.toString(arrayStart)); 
+        
+        int elementToSearch = ScannerNumber();
+        int result = binarySearch(arrayStart, elementToSearch);
+        if (result >= 0) System.out.println("Искомый элемент " +  elementToSearch + " имеет индекс " + result);
+        else System.out.println("Искомый элемент не найден"); 
     }
 }
--- HW02Task02.java
+++ HW02Task02.java
@@ -3,16 +3,32 @@ import java.util.Arrays;
 // Написать метод, который будет эмулировать работу динамического массива 
 // (Без исопользования коллекций), протестировать можно при выводе последовательности 
 // треугольного числа до n
-// Пример
-// 1
-// 1 3
-// 1 3 6
-// Tn = (n*(n+1))/2
-// 1, 2, 3, 4,  5,  6,   7,  8,  9, 10, 11, 12, 13, 14, 15... ЧИСЛО
-// 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105,120 ...ТРЕУГОЛЬНОЕ ЧИСЛО
+    // Пример
+    // 1
+    // 1 3
+    // 1 3 6
+        // Tn = (n*(n+1))/2
+        // 1, 2, 3, 4,  5,  6,   7,  8,  9, 10, 11, 12, 13, 14, 15... ЧИСЛО
+        // 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105,120 ...ТРЕУГОЛЬНОЕ ЧИСЛО
 
+
+
+        // ВЫВОД
+            // Последовательность треугольного числа для чисел от 5 до 15
+            // [15]
+            // [15, 21]
+            // [15, 21, 28]
+            // [15, 21, 28, 36]
+            // [15, 21, 28, 36, 45]
+            // [15, 21, 28, 36, 45, 55]
+            // [15, 21, 28, 36, 45, 55, 66]
+            // [15, 21, 28, 36, 45, 55, 66, 78]
+            // [15, 21, 28, 36, 45, 55, 66, 78, 91]
+            // [15, 21, 28, 36, 45, 55, 66, 78, 91, 105]
+            // [15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120]
+            
 public class HW02Task02 {
-    // добавление элемента в массив
+    // добавление элемента в массив - не динамический, конечно, но не смогла сделать динамический
     public static int[] addElementToArray(int[] arrayStart, int elementToAdd) {
         int[] arrayEnd = new int[arrayStart.length + 1];
 
@@ -24,8 +40,7 @@ public class HW02Task02 {
     }
 
     // вывод последовательности треугольного числа n c заданной точкой старта
-    public static int[] triangularNumber(int min, int max) {
- 
+    public static int[] triangularNumberSequence(int min, int max) {
         int size = (max - min) + 1;
         int [] array = new int [0];
         int number = min;
@@ -33,18 +48,19 @@ public class HW02Task02 {
             while (number <= max) {
                 int result = (number * (number + 1)) / 2;
                 array = addElementToArray(array, result);
-                number++;
+                number++;        
+            System.out.println(Arrays.toString(array));
+
             }
         }
         return array;
     }
     
-    
     public static void main(String[] args) {
-        int min = 3;
-        int max = 9;
+        int min = 5;
+        int max = 15;
         System.out.println("Последовательность треугольного числа для чисел от " + min + " до " + max);
-        System.out.println(Arrays.toString(triangularNumber(min, max)));
-
+        triangularNumberSequence(min, max);
     }
 }
+
